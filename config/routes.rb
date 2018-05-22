8a8301c7cd074f6dfe48a875754d1c16d94e12f4
Yuezhi Li <clivialee@gmail.com>
2018-05-22 14:15:21 +0800

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/:user_id/items', to: "items#index"
-      get 'my_items', to: 'items#my_items'
+      get '/:user_id/my_items', to: 'items#my_items'
       get 'search/:keyword', to: 'items#search'
       post '/login', to: "login#login"
-      resource :profile, only: [:show, :update]
+      get '/:user_id/profile', to: 'profiles#show'
+      put '/:user_id/profile', to: 'profiles#update'
       resources :items, only: [:show, :create, :update, :destroy] do
         resources :connections, only: [:show, :create, :destroy]
       end
