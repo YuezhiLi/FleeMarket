5b433b67fd5a534ce696d9cac85eb74148f94048
Yuezhi Li <clivialee@gmail.com>
2018-05-22 13:12:38 +0800

    commented validation of the length of the token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :items
   has_many :connections
   has_many :wanted_items, through: :connections, source: :item
-  validates :authorization_token, length: { is: 32 }
+  # validates :authorization_token, length: { is: 32 }
 end
