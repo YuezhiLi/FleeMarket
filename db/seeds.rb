fce1eaa48d07e1698264abaabd0568d28090c7ba
Yuezhi Li <clivialee@gmail.com>
2018-05-23 16:39:57 +0800

    changed save to save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "create 3 user..."
-3.times do
-  puts "------"
-  User.create(
-  nickName: Faker::Internet.user_name,
-  city: ["Beijing", "Shanghai", "Chengdu"].sample,
-  email: Faker::Internet.free_email,
-  phone_number: Faker::PhoneNumber.cell_phone
-  )
-  puts "created a user!"
-end
-puts "3 users created!"
+
 users = User.all
 users.each do |u|
   puts "creating 2 items for #{u.nickName}..."
