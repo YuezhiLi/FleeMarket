8a971c1c29bb881087915656fbcdfbfd0755f74b
Yuezhi Li <clivialee@gmail.com>
2018-05-23 13:05:50 +0800

    T_T

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :connections
   has_many :wanted_items, through: :connections, source: :item
   validates :email, format: { with: /.+@.+\..+/ }
-  validates :phone_number, format: { with: /\d+/ }
+  validates :phoneNumber, format: { with: /\d+/ }
   # validates :authorization_token, length: { is: 32 }
 end
