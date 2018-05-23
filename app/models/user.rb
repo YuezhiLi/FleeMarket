98a92fd355d63d1ae9a1247c96bb2297c3b9ac1c
Yuezhi Li <clivialee@gmail.com>
2018-05-23 17:44:10 +0800

    uncommented validations and added allow_nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :items
   has_many :connections
   has_many :wanted_items, through: :connections, source: :item
-  # validates :email, format: { with: /.+@.+\..+/ }
-  # validates :phoneNumber, format: { with: /\d+/ }
+  validates :email, format: { with: /.+@.+\..+/ }, allow_nil: true
+  validates :phoneNumber, format: { with: /\d+/ }, allow_nil: true
   # validates :authorization_token, length: { is: 32 }
 end
