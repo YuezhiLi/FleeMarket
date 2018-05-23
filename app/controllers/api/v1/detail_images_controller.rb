6f5528eafc8761e78f7475ac0b980f7cf12a5b90
Diffat Elicka <903869629@qq.com>
2018-05-23 17:25:43 +0800

    detail image

--- app/controllers/api/v1/detail_images_controller.rb
+++ app/controllers/api/v1/detail_images_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::DetailImagesController < Api::V1::BaseController
+  def create
+    @detail_images = DetailImage.new(detail_images_params)
+  end
 
+  private
+  def detail_images_params
+    params.require(:image).permit(:url)
+  end
 end
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,6 +7,14 @@ class Api::V1::ItemsController < Api::V1::BaseController
   def show
   end
 
+  def detail_image
+    detail_images_params[:image].each do |x|
+      c = DetailImage.new(url: x)
+      c.item = @item
+      c.save
+    end
+  end
+
   def my_items
     items = Item.all
     @items = items.select { |i| i.user == @current_user}
@@ -15,6 +23,7 @@ class Api::V1::ItemsController < Api::V1::BaseController
   def create
     @item = Item.new(item_params)
     @item.user = @current_user
+    detail_image
     if @item.save
       render :show
     else
@@ -47,5 +56,9 @@ class Api::V1::ItemsController < Api::V1::BaseController
 
   def item_params
     params.require(:item).permit(:title, :condition, :cover_image, :description, :city)
+  end
+
+  def image_url
+    params.require(:item).permit(:imagePaths)
   end
 end
