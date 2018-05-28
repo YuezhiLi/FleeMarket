fc33e657af01dc8960843da756b359a342442532
Yuezhi Li <clivialee@gmail.com>
2018-05-28 11:31:38 +0800

    made destroy dependencies between models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :connections
-  has_many :detail_images
+  has_many :connections, dependent: :destroy
+  has_many :detail_images, dependent: :destroy
   acts_as_taggable_on :tags
 end
