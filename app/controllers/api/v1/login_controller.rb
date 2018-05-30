d65a921eee2978d9cd89c4b0441e48116a461bbf
Yuezhi Li <clivialee@gmail.com>
2018-05-30 19:48:08 +0800

    _

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -31,11 +31,11 @@ class Api::V1::LoginController < Api::V1::BaseController
       phoneNumber: @user.phoneNumber,
       openId: @user.open_id
       }
-    items = Item.all
-    items.each do |i|
-      i.expired = true if DateTime.now - i.updated_at >= 30
-      i.save
-    end
+    # items = Item.all
+    # items.each do |i|
+    #   i.expired = true if DateTime.now - i.updated_at >= 30
+    #   i.save
+    # end
   end
 
   private
